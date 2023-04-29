e39c459baeb6880b359ff11da23ec2aa506a57bb
Tauseef Rashid <tauseefrashid000@gmail.com>
2023-04-29 14:53:48 +0530

    new

--- script.js
+++ script.js
@@ -16,3 +16,7 @@ btnScrollToTop.addEventListener("click", function () {
     behavior: "smooth",
   });
 });
+
+
+
+{/* <div class="mapouter"><div class="gmap_canvas"><iframe width="770" height="510" id="gmap_canvas" src="https://maps.google.com/maps?q=naqashbanditravels&t=&z=10&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://2yu.co">2yu</a><br><style>.mapouter{position:relative;text-align:right;height:510px;width:770px;}</style><a href="https://embedgooglemap.2yu.co">html embed google map</a><style>.gmap_canvas {overflow:hidden;background:none!important;height:510px;width:770px;}</style></div></div> */}
